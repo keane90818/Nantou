12471850d22eb48c230681318d1c50a1e14f9227
keane90818 <124663137+keane90818@users.noreply.github.com>
2023-03-08 13:34:57 +0800

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,43 @@
+
+
+$(document).ready(function(){  
+
+    $(".headerPage").load("header.html");
+    $(".footerPage").load("footer.html");
+    
+    $(".search-button").click(function(){
+    $("input").slideToggle();
+    });
+    $(window).scroll(function(){
+        //var $(window).scrollTop(); 為 scroll
+        var scroll = $(window).scrollTop();
+        if( scroll >= 70){
+            // 當卷軸超過70px,.gototop就淡入，如果小於就淡出
+            $(".goTopButton").fadeIn();
+        }
+        else{
+            $(".goTopButton").fadeOut();   
+        }
+        });
+    // 當我按下.gototop時，添加動畫讓卷軸跑道最上面
+    $('#modalBtn').click().hide();
+    $('.goTopButton').click(function(){
+        $('html,body').animate({
+            scrollTop:$('html').offset().top
+        })
+        return false;
+    });
+    $('.nav-logo').click(function(){
+        $('html,body').animate({
+            scrollTop:$('html').offset().top
+        })
+        return false;
+    });
+   
+  });
+
+
+
+
+
+
