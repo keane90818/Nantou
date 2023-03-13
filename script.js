045052dfa594c4f1ae38e5db5de672eb0e24b0d5
keane90818 <124663137+keane90818@users.noreply.github.com>
2023-03-13 15:25:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
         }
         });
     // 當我按下.gototop時，添加動畫讓卷軸跑道最上面
-    $('#modalBtn').click().hide();
+    
+
     $('.goTopButton').click(function(){
         $('html,body').animate({
             scrollTop:$('html').offset().top
@@ -33,7 +34,81 @@ $(document).ready(function(){
         })
         return false;
     });
-   
+    $('#modalBtn1').click().hide();
+
+
+    // 點擊modal按鈕觸發動畫
+    $('#modalBtn').click(function(){
+        const b = document.getElementById('b');
+        b.classList.add('main-first-title');
+
+        const topImg = document.getElementById('topImg');
+        topImg.classList.add('animate__animated');
+        topImg.classList.add('animate__backInLeft'); 
+
+        const topicA = document.getElementById('topicA');
+        topicA.classList.add('animate__animated');
+        topicA.classList.add('animate__tada'); 
+    });
+// scroll的操作
+    let boxes = document.querySelectorAll(".box");
+    const topics1 = document.querySelectorAll(".topic1");
+    window.addEventListener("scroll", function(){
+        checkBoxes();
+        topics();
+    });
+    checkBoxes();
+    topics();
+    function topics(){
+        let triggerBottom = window.innerHeight * 0.8;
+        topics1.forEach((topic1) => {
+            let boxTop = topic1.getBoundingClientRect().top;
+            console.log(boxTop);
+            if (boxTop < triggerBottom) {
+              topic1.classList.add("animate__animated");
+              topic1.classList.add("animate__fadeInUp");
+            } else {
+              topic1.classList.remove("animate__animated");
+              topic1.classList.remove("animate__fadeInUp");
+            }
+          });
+    }
+    function checkBoxes() {
+      
+      
+      let triggerBottom = window.innerHeight * 0.8;
+      
+      boxes.forEach((box) => {
+        let boxTop = box.getBoundingClientRect().top;
+        
+        if (boxTop < triggerBottom) {
+          box.classList.add("show");
+        } else {
+          box.classList.remove("show");
+        }
+      });
+    };
+   //酒甕的特效2
+   var myButton2 = document.getElementById("beerDiv2");
+    myButton2.addEventListener("click", function() {
+  // 在這裡添加點擊按鈕後要執行的代碼
+  $('#modalBtn2').click().hide();
+
+    }   );
+    //酒甕的特效3
+   var myButton3 = document.getElementById("beerDiv3");
+   myButton3.addEventListener("click", function() {
+ // 在這裡添加點擊按鈕後要執行的代碼
+ $('#modalBtn3').click().hide();
+
+   }   );
+   //酒甕的特效4
+   var myButton4 = document.getElementById("beerDiv4");
+    myButton4.addEventListener("click", function() {
+  // 在這裡添加點擊按鈕後要執行的代碼
+  $('#modalBtn4').click().hide();
+
+    }   );
   });
 
 
